42899180434a190bca3afc104ffe821636b097a9
Parker McCullough <wpm901@gmail.com>
2017-04-21 14:05:05 -0400

    Finished bagging part of loop.

--- experiment.R
+++ experiment.R
@@ -11,9 +11,45 @@ require(randomForest)
 library(gbm)
 
 # Load Data
-DF = read.csv("DF.csv")
+DF = read.csv("data/DF.csv")
+DF$household_key=NULL
+DF$X=NULL
 summary(DF)
+head(DF)
+dim(DF)
 head(DF)
 
 # Start Analysis 
 # See homework 4 for best place to start.
+
+errmat = matrix(0,20,2)
+set.seed(2)
+
+for (i in 1:20){
+    # Split data into Training and Test sample
+  trainIndex <- sample(1:nrow(DF),400)
+  test <- DF[-trainIndex,]
+  train <- DF[trainIndex,]
+    
+  # Bagging
+  BagMod <- randomForest(TotalValue~.,data=train,mtry=14)
+  pred <- predict(BagMod,type = "response")
+  BagMean.train <- mean((train[,14] - pred)^2)
+  pred <- predict(BagMod,test,type="response")
+  BagMean.test <- mean((train[,14] - pred)^2)
+  Bag.err <- c(BagMean.train,BagMean.test)
+    
+  errmat[i,] = c(BagMean.train,BagMean.test)
+}
+
+# plots
+labels = c(rep("BagTraining",20),
+                     rep("BagTesting",20))
+err = c(errmat)
+boxplot(err~labels,ylab="Misclassification Rate")
+
+Total=sum(DF$TotalValue)
+Total
+sqrt(errmat)/Total*100
+mean(sqrt(errmat[,1]))
+mean(sqrt(errmat[,2]))
