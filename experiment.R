d1448bae2b5ad5e40509b3b67f3926ab6d088401
Parker McCullough <wpm901@gmail.com>
2017-04-25 17:31:29 -0400

    added trees models to experiment.R

--- experiment.R
+++ experiment.R
@@ -11,7 +11,8 @@ require(randomForest)
 library(gbm)
 
 # Load Data
-DF = read.csv("data/DF.csv")
+#DF = read.csv("data/DF.csv")
+DF = read.csv("DF_wo_outliers.csv")
 DF$household_key=NULL
 DF$X=NULL
 summary(DF)
@@ -21,35 +22,40 @@ head(DF)
 
 # Start Analysis 
 # See homework 4 for best place to start.
-
-errmat = matrix(0,20,2)
+n = 50
+errmat = matrix(0,n,4)
 set.seed(2)
 
-for (i in 1:20){
+for (i in 1:n){
     # Split data into Training and Test sample
   trainIndex <- sample(1:nrow(DF),400)
   test <- DF[-trainIndex,]
   train <- DF[trainIndex,]
     
   # Bagging
-  BagMod <- randomForest(TotalValue~.,data=train,mtry=14)
+  BagMod <- randomForest(TotalValue~.,data=train,mtry=8)
   pred <- predict(BagMod,type = "response")
-  BagMean.train <- mean((train[,14] - pred)^2)
-  pred <- predict(BagMod,test,type="response")
-  BagMean.test <- mean((train[,14] - pred)^2)
+  BagMean.train <- mean((train[,9] - pred)^2)
+  pred <- predict(BagMod,test[,1:8],type="response")
+  BagMean.test <- mean((test[,9] - pred)^2)
   Bag.err <- c(BagMean.train,BagMean.test)
     
-  errmat[i,] = c(BagMean.train,BagMean.test)
+  # Trees
+  tree.boston=tree(TotalValue ~ ., data=train)
+  cv.boston = cv.tree(tree.boston)
+  prune.boston = prune.tree(tree.boston)
+  trees.pred.train = predict(tree.boston)
+  trees.err.train = mean((train[,9] - trees.pred.train)^2)
+  trees.pred.test <- predict(tree.boston,newdata = test[,1:8])
+  trees.err.test <- mean((test[,9] - trees.pred.test)^2)
+  trees.err <- c(trees.err.train,trees.err.test)
+  
+  errmat[i,] = c(Bag.err, trees.err)
 }
 
 # plots
-labels = c(rep("BagTraining",20),
-                     rep("BagTesting",20))
-err = c(errmat)
+labels = c(rep("BagTraining",n), rep("BagTesting",n),
+           rep("TreesTraining",n), rep("TreesTesting",n))
+err = sqrt(c(errmat))
 boxplot(err~labels,ylab="Misclassification Rate")
 
-Total=sum(DF$TotalValue)
-Total
-sqrt(errmat)/Total*100
-mean(sqrt(errmat[,1]))
-mean(sqrt(errmat[,2]))
